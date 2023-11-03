748f23793d587ac84af9a94622f95d4bf8b28974
o-agustinSilva <agustinsilva1009@gmail.com>
2023-11-03 16:07:03 -0300

    Se creo la pantalla para solicitar un tramite

--- tramite/frontend/src/components/Home.js
+++ tramite/frontend/src/components/Home.js
@@ -1,49 +1,7 @@
-import React, { useState } from 'react';
+import React, { Component } from "react";
 
-function Login() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState(null);
-
-  const handleLogin = async (e) => {
-    e.preventDefault();
-
-    // Aquí puedes agregar la lógica para enviar las credenciales al servidor (por ejemplo, a través de una API).
-    // En este ejemplo, se simula un inicio de sesión incorrecto.
-    if (username === 'usuario' && password === 'contraseña') {
-      // Inicio de sesión exitoso, puedes redirigir al usuario a la página principal.
-      // Aquí podrías usar react-router-dom para la navegación.
-      console.log('Inicio de sesión exitoso');
-    } else {
-      setError('Credenciales incorrectas. Por favor, inténtalo de nuevo.');
-    }
-  };
-
-  return (
-    <div>
-      <h2>Iniciar Sesión</h2>
-      <form onSubmit={handleLogin}>
-        <div>
-          <label>Nombre de Usuario:</label>
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </div>
-        <div>
-          <label>Contraseña:</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button type="submit">Iniciar Sesión</button>
-      </form>
-      {error && <p>{error}</p>}
-    </div>
-  );
+function Home() {
+  return <h1>Holu</h1>
 }
 
-export default Login;
+export default Home;
--- tramite/frontend/src/components/Tramite.js
+++ tramite/frontend/src/components/Tramite.js
@@ -1,17 +1,49 @@
 import React, { Component } from "react";
 import Header from "./Header";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import { Link } from "react-router-dom";
+import Button from 'react-bootstrap/Button';
 
-export default class Tramite extends Component {
-    constructor(props) {
-        super(props);
-    }
+export function Tramite() {
+    return (
+        <div>
+            <Header/>
+            <Container fluid> 
+                <Row id="containerTramite"> 
+                    <Col className="bg-dark.bg-gradient">
+                        <h1>Imágen y título</h1>
+                    </Col>
+                </Row>
+            </Container>
 
-    render() {
-        return (
-            <div>
-                <Header/>
-                <p>This is the "tramite" page</p>
-            </div>
-        )
-    }
+            <Container>
+                <Row className="listOfTramites mt-3">
+                    <Col className="tramiteIcon h-50 w-50 d-flex justify-content-start align-items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="80" height="90" fill="#f8f4f4" className="bi bi-file-text-fill" viewBox="0 0 16 16">
+                            <path d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM5 4h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1zm-.5 2.5A.5.5 0 0 1 5 6h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5zM5 8h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1zm0 2h3a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1z"/>
+                        </svg>
+                             
+                    </Col>
+
+                    <Col xs={6} className="d-flex justify-content-center align-items-center">
+                        <h1 id="tramiteTitle">Certificado de domicilio</h1>   
+                    </Col>
+
+                    <Col className="d-flex justify-content-end align-items-center mb-3">
+                        <Form>
+                            <Link to="/solicitarTramite">
+                                <Button id="requestTramite" variant="outline-success">Solicitar</Button>
+                            </Link>
+                        </Form>
+                    </Col>
+                </Row>   
+            </Container>
+            
+        </div>
+    )
 }
+
+export default Tramite;
